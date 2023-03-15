39c5934affb9c299ccac7d9d79ed3e1d04f1b6fd
franquesino <you@example.com>
2023-03-15 13:18:33 -0300

    11

--- src/components/redux/actions.js
+++ src/components/redux/actions.js
@@ -1,9 +1,20 @@
-import { addFavorite, deleteFavorite } from "./action-types";
+import { ADD_FAVORITE, DELETE_FAVORITE, FILTER, ORDER } from "./action-types";
 
 export const addFavorite = (character) => {
-    return { type: addFavorite, payload: character }
+    return { type: ADD_FAVORITE, payload: character }
 }
 
 export const deleteFavorite = (id) => {
-    return { type: deleteFavorite, payload: id }
+    return { type: DELETE_FAVORITE, payload: id }
+}
+
+
+export const filterCards = (gender) => {
+    return { type: FILTER, payload: gender }
+
+}
+
+export const orderCards = (id) => {
+    return { type: ORDER, payload: id }
+
 }
--- src/components/redux/reducer.js
+++ src/components/redux/reducer.js
@@ -1,26 +1,45 @@
-import { addFavorite,deleteFavorite } from "./action-types";
+import { ADD_FAVORITE, DELETE_FAVORITE, FILTER, ORDER } from "./action-types";
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharacters: []
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
-        case addFavorite:
+        case ADD_FAVORITE:
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, action.payload]
+                myFavorites: [...state.allCharacters, action.payload],
+                allCharacters: [...state.allCharacters, action.payload]
             }
-        case deleteFavorite:
+        case DELETE_FAVORITE:
             return {
                 ...state,
                 myFavorites: state.myFavorites.filter(char => char.id !==
                     action.payload)
             }    
+        case FILTER:
+        
+            const allCharsFiltered = allCharacters.filter(char => char.gender === action.payload);
+            return{
+                ...state,
+                myFavorites: allCharsFiltered
+            }
+        case ORDER:
+            return {
+                ...state,
+            myFavorites:
+                action.payload === 'Ascendente'
+                ? state.allCharacters.sort((a, b) => a.id - b.id)
+                : state.allCharacters.sort((a, b)=> b.id - a.id)
+        }    
+
+
 
         default:
-            return {...state}
-    }
+            return {
+                ...state }
 }
-
+}
 export default reducer;
